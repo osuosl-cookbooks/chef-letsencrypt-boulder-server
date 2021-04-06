0657a5a3f508b3d61f7d2765014575aad189f6f2
Ryan <kennedy@osuosl.org>
2021-04-05 17:38:10 -0700

    Update Chef dependency to >= 16

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'osl-letsencrypt-boulder-server'
 maintainer       'Oregon State University'
 maintainer_email 'chef@osuosl.org'
 license          'Apache-2.0'
-chef_version     '>= 14'
+chef_version     '>= 16.0'
 description      "Installs/Configures Boulder, the ACME-based CA server by Let's Encrypt."
 issues_url       'https://github.com/osuosl-cookbooks/osl-letsencrypt-boulder-server/issues'
 source_url       'https://github.com/osuosl-cookbooks/osl-letsencrypt-boulder-server'
